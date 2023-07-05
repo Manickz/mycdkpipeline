e4f1868b96b886b8f77a648f9bc6a298d2f11f56
manubam <manickamsp@gmail.com>
2023-07-05 23:12:20 +0530

    added california

--- lib/stages/devdemek-stage.ts
+++ lib/stages/devdemek-stage.ts
@@ -12,7 +12,7 @@ export class DevDemeKStage extends Stage {
     // ADD AMPLIFY EXPORTED BACKEND STACK HERE
     const amplifyStack = new AmplifyExportedBackend(this, "devdemekexportedbackend", {
       path: path.resolve(__dirname, '..', 'amplify-export-DevDemeK'),
-      amplifyEnvironment: "dev-london"
+      amplifyEnvironment: "dev-california"
     })
   }
 }
